b45594e56e7dac13ed9729b96d03d05f0c684c43
ehlera1 <adam.p.ehlers@gmail.com>
2021-12-04 10:44:07 -0600

    Mod Complete

--- static/js/app.js
+++ static/js/app.js
@@ -48,5 +48,10 @@ function handleClick() {
     buildTable(filteredData);
   };
 
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
 
  
